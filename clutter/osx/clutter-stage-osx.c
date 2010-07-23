6d08bbf5d1e20e529d890044b775561c70774f8b
Roman Kudiyarov <rkudiyarov@crystalnix.com>
2010-07-23 11:25:40 +0700

    Fixed lack of reaction for keyboard events after showing stage on osx.
    Trick with hiding view while showing the stage affects on responder
    chain. The main view ceases to be first responder and we should
    manually set first responder.

--- clutter/osx/clutter-stage-osx.c
+++ clutter/osx/clutter-stage-osx.c
@@ -333,18 +333,20 @@ clutter_stage_osx_show (ClutterStageWindow *stage_window,
   clutter_actor_map (CLUTTER_ACTOR (self->wrapper));
 
   clutter_stage_osx_set_frame (self);
-  /* draw view should be avoided and it is the reason why
+  /* Draw view should be avoided and it is the reason why
      we should hide OpenGL view while we showing the stage.
   */
   BOOL isViewHidden = [self->view isHidden];
   if ( isViewHidden == NO)
     {
       [self->view setHidden:YES];
-    }
+    } 
   [self->window makeKeyAndOrderFront: nil];
   [self->view setHidden:isViewHidden];
-
-  [self->window makeKeyAndOrderFront: nil];
+  /*
+   * After hiding we cease to be first responder.
+   */
+  [self->window makeFirstResponder: self->view];
 
   CLUTTER_OSX_POOL_RELEASE();
 }
